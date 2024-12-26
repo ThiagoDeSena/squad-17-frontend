87ff59de3775caaacf0ffc69142e9dd4c72c6b16
Wallace Santana <wallacesantanak0@gmail.com>
2024-12-26 14:43:57 -0300

    fix: configura requisições da API

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: "https://api.themoviedb.org/3",
     headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`,
         "Content-Type": "application/json;charset=utf-8",
